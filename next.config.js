305c4629edb146f9fd28a994d61efbb3b5a94fc1
ryukiller <rian.rodrigo.webmaster@gmail.com>
2023-06-02 23:13:13 +0200

    trying pwa

--- next.config.js
+++ next.config.js
@@ -12,3 +12,14 @@ module.exports = {
     return config;
   },
 };
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true
+})
--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-slate-600">
+      <head>
+      <link
+          rel="manifest"
+          href="/manifest.json"
+      />
+    </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
